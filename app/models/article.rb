1b010df3fd770069dab0bf759cb038c69a6246a0
Allen <rennyallen@hotmail.com>
2016-09-27 10:47:38 +0800

    dependent

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
-	validates :title, presence: true
+	validates :title, presence: true, dependent: :destroy
 	validates :content, presence: true, length: { minimum: 5 }
 end
