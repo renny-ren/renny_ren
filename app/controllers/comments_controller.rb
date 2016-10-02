b548085c06d8a93ff0f98aca89aae64a8675aeaa
Allen <rennyallen@hotmail.com>
2016-10-02 22:28:49 +0800

    Update comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,6 @@ class CommentsController < ApplicationController
 		@comment = @article.comments.create(comment_params)
 		redirect_to article_path(@article)
 	end
-
 	def destroy
 		@article = Article.find(params[:article_id])
 		@comment = @article.comments.find(params[:id])
