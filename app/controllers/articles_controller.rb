f8218f0e1512f249de8f6f542991c105530e889c
Allen <rennyallen@hotmail.com>
2016-10-08 22:41:28 +0800

    Update articles_controller.rb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,15 +16,15 @@ class ArticlesController < ApplicationController
 	end
 
 	def show
-		@article = Article.find(params[:id])
+	  @article = Article.find(params[:id])
 	end
 
 	def edit
-		@article = Article.find(params[:id])
+	  @article = Article.find(params[:id])
 	end
 
 	def update
-		@article = Article.find(params[:id])
+	  @article = Article.find(params[:id])
 
 		if @article.update(article_params)
 		  redirect_to @article
