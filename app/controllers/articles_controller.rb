6c1625b81426e7e5ea5aaaa712e91c49a16507fe
Allen <rennyallen@hotmail.com>
2016-10-09 22:15:50 +0800

    Update articles_controller.rb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,9 +14,9 @@ class ArticlesController < ApplicationController
     end
   end
 
-	def show
-	  @article = Article.find(params[:id])
-	end
+  def show
+    @article = Article.find(params[:id])
+  end
 
 	def edit
 	  @article = Article.find(params[:id])
