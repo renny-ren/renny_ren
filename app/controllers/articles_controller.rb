8cacbadc291d370c075365210b67ddfd1ad5fbae
Allen <rennyallen@hotmail.com>
2016-10-23 20:24:38 +0800

    Update articles_controller.rb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -42,7 +42,7 @@ class ArticlesController < ApplicationController
   redirect_to articles_path
   end
 
-	private
+  private
 		def article_params
 			params.require(:article).permit(:title, :content)
 		end
