8dca33af461c9e0438e32a8df8e893858c6d8c46
rennyallen <rennyallen@hotmail.com>
2017-03-09 17:58:17 +0800

    fix bug

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( TweenMax.min.js CustomEase.min.js huochai.js )
+Rails.application.config.assets.precompile += %w( TweenMax.min.js CustomEase.min.js huochai.js jR3DCarousel.min.js carousel.js typed.js )
