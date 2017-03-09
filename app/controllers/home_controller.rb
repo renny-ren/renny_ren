eceae36895409dbd3326e8e961692bd7ed569973
rennyallen <rennyallen@hotmail.com>
2017-03-09 16:05:49 +0800

    change footer

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    redirect_to home_path(:locale => params[:set_locale]) if params[:set_locale]
+    redirect_to locale: params[:set_locale] if params[:set_locale]
   end
 end
