cd1ceaebe4e2d72b829ae46d8f70595e32ead298
rennyallen <rennyallen@hotmail.com>
2017-03-12 20:50:24 +0800

    mobile css

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,4 +1,4 @@
-# I18n.config.available_locales = :ch
-I18n.default_locale = :en
+I18n.config.available_locales = [:ch, :en, :ja]
+I18n.default_locale = :ch
 
 LANGUAGES = [ ['中文', 'ch'], ['English', 'en'], ['にほんご', 'ja'] ]
