28de8e24f7b5baaca2fc697fa079c9e12c3e3a57
rennyallen <rennyallen@hotmail.com>
2017-03-16 18:32:57 +0800

    added contact

---
+++ app/assets/javascripts/window.js
@@ -0,0 +1,9 @@
+function openWindow(){
+  document.getElementById('wechat').style.display = 'block';
+  document.getElementById('black_overlay').style.display='block';
+}
+
+function closeWindow(){
+  document.getElementById('wechat').style.display = 'none';
+  document.getElementById('black_overlay').style.display='none';
+}
