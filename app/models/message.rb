fe0485d7a23b70bfea5bda0e8cd34c340fa3b927
rennyallen <rennyallen@hotmail.com>
2018-04-23 23:55:01 +0800

    add restriction

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,7 @@
 class Message < ApplicationRecord
   validates_presence_of :body
+  validates :body, length: { maximum: 50 }
+  validates :author, length: { maximum: 15 }
 
   def self.send_feedback_email(body, contact_info)
     MessageMailer.feedback_email(body, contact_info).deliver_later
