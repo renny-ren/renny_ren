8383f21ff8f4fb4591df955181d167d9fbf30ef1
Renny Ren <renny.ren@strikingly.com>
2020-03-14 15:48:23 +0800

    temporarily remove slider show

--- app/assets/javascripts/carousel.js
+++ app/assets/javascripts/carousel.js
@@ -1,56 +1,56 @@
-$(document).ready(function(){
-  var slides = [{src: '/basketball.jpg'}, {src: '/basketball2.jpg'}, {src: '/dormitory.jpg'}, {src: '/dormitory2.jpg'}, {src: '/rayallen.jpg'}]
-  var jR3DCarousel;
-  var carouselProps =  {
-            width: 502,         /* largest allowed width */
-            height: 350,        /* largest allowed height */
-            slideLayout : 'fill',     /* "contain" (fit according to aspect ratio), "fill" (stretches object to fill) and "cover" (overflows box but maintains ratio) */
-            animation: 'slide',     /* slide | scroll | fade | zoomInSlide | zoomInScroll */
-            animationCurve: 'ease',
-            animationDuration: 700,
-            animationInterval: 2800,       //slideClass: 'jR3DCarouselCustomSlide',
-            autoplay: true,
-            onSlideShow: show,    /* callback when Slide show event occurs */
-            navigation: 'circles',  /* circles | squares */
-            slides: slides      /* array of images source or gets slides by 'slide' class */
-        }
-  function setUp(){
-    jR3DCarousel = $('.jR3DCarouselGallery').jR3DCarousel(carouselProps);
+// $(document).ready(function(){
+//   var slides = [{src: '/basketball.jpg'}, {src: '/basketball2.jpg'}, {src: '/dormitory.jpg'}, {src: '/dormitory2.jpg'}, {src: '/rayallen.jpg'}]
+//   var jR3DCarousel;
+//   var carouselProps =  {
+//             width: 502,         /* largest allowed width */
+//             height: 350,        /* largest allowed height */
+//             slideLayout : 'fill',     /* "contain" (fit according to aspect ratio), "fill" (stretches object to fill) and "cover" (overflows box but maintains ratio) */
+//             animation: 'slide',     /* slide | scroll | fade | zoomInSlide | zoomInScroll */
+//             animationCurve: 'ease',
+//             animationDuration: 700,
+//             animationInterval: 2800,       //slideClass: 'jR3DCarouselCustomSlide',
+//             autoplay: true,
+//             onSlideShow: show,    /* callback when Slide show event occurs */
+//             navigation: 'circles',  /* circles | squares */
+//             slides: slides      /* array of images source or gets slides by 'slide' class */
+//         }
+//   function setUp(){
+//     jR3DCarousel = $('.jR3DCarouselGallery').jR3DCarousel(carouselProps);
 
-    $('.settings').html('<pre>$(".jR3DCarouselGallery").jR3DCarousel('+JSON.stringify(carouselProps, null, 4)+')</pre>');   
+//     $('.settings').html('<pre>$(".jR3DCarouselGallery").jR3DCarousel('+JSON.stringify(carouselProps, null, 4)+')</pre>');   
     
-  }
-  function show(slide){
-    console.log("Slide shown: ", slide.find('img').attr('src'))
-  }
-  $('.carousel-props input').change(function(){
-    if(isNaN(this.value))
-      carouselProps[this.name] = this.value || null; 
-    else
-      carouselProps[this.name] = Number(this.value) || null; 
+//   }
+//   function show(slide){
+//     console.log("Slide shown: ", slide.find('img').attr('src'))
+//   }
+//   $('.carousel-props input').change(function(){
+//     if(isNaN(this.value))
+//       carouselProps[this.name] = this.value || null; 
+//     else
+//       carouselProps[this.name] = Number(this.value) || null; 
     
-    for(var i = 0; i < 999; i++)
-       clearInterval(i);
-    $('.jR3DCarouselGallery').empty();
-    setUp();
-    jR3DCarousel.showNextSlide();
-  })
+//     for(var i = 0; i < 999; i++)
+//        clearInterval(i);
+//     $('.jR3DCarouselGallery').empty();
+//     setUp();
+//     jR3DCarousel.showNextSlide();
+//   })
   
-  $('[name=slides]').change(function(){
-    carouselProps[this.name] = getSlides(this.value); 
-    for (var i = 0; i < 999; i++)
-       clearInterval(i);
-    $('.jR3DCarouselGallery').empty();
-    setUp();
-    jR3DCarousel.showNextSlide();   
-  });
+//   $('[name=slides]').change(function(){
+//     carouselProps[this.name] = getSlides(this.value); 
+//     for (var i = 0; i < 999; i++)
+//        clearInterval(i);
+//     $('.jR3DCarouselGallery').empty();
+//     setUp();
+//     jR3DCarousel.showNextSlide();   
+//   });
   
-  function getSlides(no){
-    slides = [];
-    for ( var i = 0; i < no; i++) {
-      slides.push({src: 'https://unsplash.it/'+Math.floor(1366-Math.random()*200)+''+Math.floor(768+Math.random()*200)})
-    }
-    return slides;
-  }
-  setUp()
-})
+//   function getSlides(no){
+//     slides = [];
+//     for ( var i = 0; i < no; i++) {
+//       slides.push({src: 'https://unsplash.it/'+Math.floor(1366-Math.random()*200)+''+Math.floor(768+Math.random()*200)})
+//     }
+//     return slides;
+//   }
+//   setUp()
+// })
