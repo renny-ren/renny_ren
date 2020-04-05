0d928a455b6361d614b996154ed0b461be4789a2
Renny Ren <renny.ren@strikingly.com>
2020-04-05 23:35:34 +0800

    about page Chinese version

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,3 +1,5 @@
 class AboutController < ApplicationController
-  def index; end
+  def index
+    @slides = Dir['public/carousel_images/*'].sort.map { |img| { src: "/#{File.basename(img)}" } }
+  end
 end
