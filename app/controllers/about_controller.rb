bc9b93771abe7e87744c8ea44612a89e38b7d7c6
Renny Ren <renny.ren@strikingly.com>
2020-04-06 16:27:48 +0800

    add cdn host

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,5 +1,5 @@
 class AboutController < ApplicationController
   def index
-    @slides = Dir['public/carousel_images/*'].sort.map { |img| { src: "/carousel_images/#{File.basename(img)}" } }
+    @slides = Dir['public/carousel_images/*'].sort.map { |img| { src: "#{Settings.cdn_host}/carousel_images/#{File.basename(img)}" } }
   end
 end
