71b3363b601fc87b5392ca09afd4eb9350cc1412
Renny Ren <renny.ren@strikingly.com>
2020-04-23 13:01:58 +0800

    feat: search videos - part 2

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,4 +2,6 @@ class Video < ApplicationRecord
   acts_as_taggable
 
   has_many :comments, as: :owner, dependent: :destroy
+
+  scope :search_filter, -> (query) { where('title LIKE ? OR description LIKE ?', "%#{query}%", "%#{query}%") }
 end
