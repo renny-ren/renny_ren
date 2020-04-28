6c6b5e6eefa4654e76451c1aac363345f7462ad3
Renny Ren <renny.ren@strikingly.com>
2020-04-28 23:58:47 +0800

    feat: config ckeditor for comment - part 3

--- app/assets/javascripts/ckeditor/config_for_comment.js
+++ app/assets/javascripts/ckeditor/config_for_comment.js
@@ -1,18 +1,21 @@
 if (typeof(CKEDITOR) !== 'undefined') {
   CKEDITOR.plugins.addExternal( 'emoji', '/assets/ckeditor/plugins/emoji/plugin.js' );
   CKEDITOR.plugins.addExternal( 'wordcount', '/assets/ckeditor/plugins/wordcount/plugin.js' );
+  CKEDITOR.plugins.addExternal( 'confighelper', '/assets/ckeditor/plugins/confighelper/plugin.js' );
 
   CKEDITOR.editorConfig = function( config ) {
     // config.uiColor = '#f0f8ff';
-    config.extraPlugins = 'emoji, wordcount, autogrow';
-    config.autoGrow_minHeight = 140;
+    config.extraPlugins = 'emoji, wordcount, autogrow, confighelper';
+    config.height = 120;
+    config.autoGrow_minHeight = 120;
     config.autoGrow_maxHeight = 500;
     config.emoji_emojiListUrl = '/assets/ckeditor/plugins/emoji/emoji.json';
     config.wordcount = {
       showParagraphs: true,
       showWordCount: true,
       showCharCount: true,
-      maxCharCount: 40,
+      maxCharCount: 400,
+      pasteWarningDuration: 2000,
     }
     config.removePlugins = 'elementspath';
     config.resize_enabled = false;
