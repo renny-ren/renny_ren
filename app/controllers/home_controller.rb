529a585fa23067900c8fc6d650ebcb6a407856ec
Renny Ren <junhong.ren@longbridge.sg>
2021-04-17 14:50:24 +0800

    fix N+1

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,7 @@
 class HomeController < ApplicationController
   def index
     @messages = Message.order('id DESC')
-    @sentence = Sentence.last
-    @timeline_items = TimelineItem.order(created_at: :desc).paginate(page: params[:page])
+    @timeline_items = TimelineItem.includes(:owner).order(created_at: :desc).paginate(page: params[:page])
     redirect_to locale: params[:set_locale] if params[:set_locale]
 
     respond_to do |format|
