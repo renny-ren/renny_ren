61d6b265263b51a011198739ef39232b0cde9ee7
Renny Ren <rennyallen@hotmail.com>
2023-08-16 22:56:19 +0800

    tweak: timeline items per page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
-    @messages = Message.order('id DESC')
-    @timeline_items = TimelineItem.includes(:owner).order(created_at: :desc).paginate(page: params[:page])
+    @messages = Message.order("id DESC")
+    @timeline_items = TimelineItem.includes(:owner).order(created_at: :desc).paginate(page: params[:page], per_page: 15)
     redirect_to locale: params[:set_locale] if params[:set_locale]
 
     respond_to do |format|
